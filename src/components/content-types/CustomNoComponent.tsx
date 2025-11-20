f17f70d46e839932a2dfa40bf8412932bc2f3d56
AP2300 <andresparedes202@gmail.com>
2025-11-20 10:54:26 -0400

    fix: remove background color from CustomNoComponent for consistency

--- src/components/content-types/CustomNoComponent.tsx
+++ src/components/content-types/CustomNoComponent.tsx
@@ -4,7 +4,7 @@ interface CustomNoComponentProps {
 
 export function CustomNoComponent({ contentType }: CustomNoComponentProps) {
   return (
-    <div className="relative w-full bg-gray-200 h-12 flex justify-center items-center overflow-hidden">
+    <div className="relative w-full h-12 flex justify-center items-center overflow-hidden">
       Dont have a component for this&nbsp;<strong>{contentType}</strong>.
     </div>
   );
