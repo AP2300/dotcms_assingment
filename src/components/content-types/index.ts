efff643f35ac43f93a41464e5f6d9e112750c073
AP2300 <andresparedes202@gmail.com>
2025-11-20 10:50:25 -0400

    feat: add CustomNoComponent to contentTypesComponents for handling missing components

--- src/components/content-types/index.ts
+++ src/components/content-types/index.ts
@@ -1,6 +1,7 @@
 import ActivityCard from "./Activity";
+import { CustomNoComponent } from "./CustomNoComponent";
 import Hero from "./Hero";
 import Product from "./Product";
 import YouTube from "./Youtube";
 
-export const contentTypesComponents = { Banner: Hero, Activity: ActivityCard, Product: Product, YouTube: YouTube };
+export const contentTypesComponents = { Banner: Hero, Activity: ActivityCard, Product: Product, YouTube: YouTube, CustomNoComponent: CustomNoComponent };
